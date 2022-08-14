41c65cc6fc734d9d4ec988bbd8073b72eb65f81e
samridh <samridht23@gmail.com>
2022-08-14 13:40:14 +0530

    new

--- src/routes/Home.jsx
+++ src/routes/Home.jsx
@@ -5,7 +5,7 @@ const Home = () => {
   return (
     <Box>
       <Flex>
-        <Text fontSize={{ base: "2xl", sm: "4xl", lg: "5xl" }}>
+        <Text as={"b"} fontSize={{ base: "2xl", sm: "4xl", lg: "5xl" }}>
           Product Designer creating thoughtful, intutive interfaces
         </Text>
       </Flex>
